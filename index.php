b6866fe08e645600579450fdca3e8a2cd6326ee7
JohnyRamonSSousa <erikaandradesantosousa@gmail.com>
2025-11-20 22:47:39 -0300

    terceiro commit

--- index.php
+++ index.php
@@ -2,8 +2,41 @@
  include_once("templates/header.php");
 ?>
     <!-- Bootstrap JS -->
-    <h1>Testando css</h1>
-    <i class="fas fa-sync-alt"></i>
+    <div id="main-banner">
+        <h1>Faça seu pedido</h1>
+    </div>
+    <div id="main-container">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Monte a pizza como desejar:</h2>
+                    <form action="/process/pizza.php" method="post" id="pizza-form">
+                        <div class="form-grup">
+                            <label for="borda">Borda</label>
+                            <select name="borda" id="borda" class="form-control">
+                                <option value="">Selecione a borda</option>
+                            </select>
+                        </div>
+                        <div class="form-grup">
+                            <label for="massa">Massa</label>
+                            <select name="massa" id="massa" class="form-control">
+                                <option value="">Selecione a massa</option>
+                            </select>
+                        </div>
+                        <div class="form-grup">
+                            <label for="sabores">Sabores: (Máximo 3)</label>
+                            <select multiple name="sabores[]" id="sabores" class="form-control">
+                                
+                            </select>
+                        </div>
+                        <div class="form-grup">
+                            <input type="submit" class="btn btn-primary" value="Fazer Pedido!">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 <?php
 
  include_once("templates/footer.php");
