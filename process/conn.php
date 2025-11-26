d406c11e1b7cfdf3882b0a21abb59795fc5cb7ea
JohnyRamonSSousa <erikaandradesantosousa@gmail.com>
2025-11-25 22:26:41 -0300

    projeto finalizado

--- process/conn.php
+++ process/conn.php
@@ -1,22 +1,23 @@
 <?php
- session_start();
 
- $user = "root";
- $pass = "";
- $db = "pizzaria";
- $host = "localhost";
+  session_start();
 
- try {
+  $user = "root";
+  $pass = "";
+  $db = "pizzaria";
+  $host = "localhost";
 
-   $conn = new PDO("mysql:host={$host};dbname={$db}",$user, $pass);
-   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-   $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+  try {
 
- } catch(PDOException $e){
-   print "Erro" . $e->getMessage() . "<br/>";
-   die();
+    $conn = new PDO("mysql:host={$host};dbname={$db}", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
- }
+  } catch (PDOException $e) {
 
+    print "Erro: " . $e->getMessage() . "<br/>";
+    die();
 
-?> 
+  }
+
+?>
